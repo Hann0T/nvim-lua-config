aa3461553755b9f46337cf102ca07f3a1d2b83d6
Hann0T <hansolivas1@gmail.com>
2023-01-21 17:34:20 -0500

    chore: neodev.

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -125,6 +125,11 @@ _G.packer_plugins = {
     path = "/Users/hansolivas/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["neodev.nvim"] = {
+    loaded = true,
+    path = "/Users/hansolivas/.local/share/nvim/site/pack/packer/start/neodev.nvim",
+    url = "https://github.com/folke/neodev.nvim"
+  },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
