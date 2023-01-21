6212feb8eba85f93c8a1f4fb70c10e88ff8bb897
Hann0T <hansolivas1@gmail.com>
2023-01-21 15:31:13 -0500

    refactor!: TreeSitter plugin.
    - if the TreeSitter plugin doesn't work after an update, execute the
      :TSUpdate

--- lua/hann0t/packer.lua
+++ lua/hann0t/packer.lua
@@ -11,9 +11,13 @@ return require('packer').startup(function(use)
     use 'folke/tokyonight.nvim'
 
     use 'nvim-lua/plenary.nvim'
-    use("nvim-treesitter/nvim-treesitter", {
-        run = ":TSUpdate"
-    })
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
     use 'nvim-treesitter/nvim-treesitter-context'
     -- Super useful
     use {
