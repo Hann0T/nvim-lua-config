c150d086a5304ec2e39e2b470a8d060a4e6793b8
hann0t <hansolivas1@gmail.com>
2024-01-08 15:20:52 -0500

    chore(deps): telescope was updated.

--- lua/hann0t/packer.lua
+++ lua/hann0t/packer.lua
@@ -21,7 +21,8 @@ return require('packer').startup(function(use)
     use 'nvim-treesitter/nvim-treesitter-context'
     -- Super useful
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
     use 'ThePrimeagen/harpoon'
 
